9c51950237a1fcea3ca47bf0779fb29a4423deb1
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-08-24 00:33:43 +0800

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// Scripts for the component
+</script>
+
+<template>
+  <div class="container">
+    <!-- HTML elements for the component -->
+    <h1 id="game-title">Guess The Number: The Video Game</h1>
+    <img id="thonk-gif" src="./assets/thonk.gif" />
+  </div>
+</template>
+
+<style scoped>
+/* CSS styles for the component */
+
+#game-title {
+  font-size: 4vw;
+}
+
+#thonk-gif {
+  width: 30%;
+}
+</style>
